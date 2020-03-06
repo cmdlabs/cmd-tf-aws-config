737e65ec152a7e67d5351e4917f41974f48a114b
Daniel Navarro <danielnavarro@Daniels-MacBook-Pro.local>
2020-03-06 14:20:27 +1100

    Update tests

--- tests/main.tf
+++ tests/main.tf
@@ -1,20 +1,45 @@
 module "aggregator" {
   source = "../"
   
-  aggregator_account_id     =   "722141136946"
+  aggregator_account_id     =   "354334841216"
   aggregator_account_region =   "ap-southeast-2"
-  bucket_name   =   "cmdlab-sandpit1-${random_string.random.result}"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+  is_aggregator = true
 }
 
 module "bucket" {
   source = "../"
-  bucket_name   =   "cmdlab-sandpit1-${random_string.random.result}"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+}
+
+module "recorder" {
+  source = "../"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+}
+
+module "role" {
+  source = "../"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+  
+}
+
+module "rules" {
+  source = "../"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+  
+}
+
+module "sns" {
+  source = "../"
+  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
+  
 }
 
 
 resource "random_string" "random" {
     length  =   8
     special =   false
+    upper   =   false
   
 }
 
