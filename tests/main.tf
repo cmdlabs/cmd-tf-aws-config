fc0d068b3fa9ec14081fa510345b741068fec815
Daniel Navarro <danielnavarro@Daniels-MacBook-Pro.local>
2020-03-06 14:42:34 +1100

    remove additional module reference

--- tests/main.tf
+++ tests/main.tf
@@ -7,35 +7,6 @@ module "aggregator" {
   is_aggregator = true
 }
 
-module "bucket" {
-  source = "../"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-}
-
-module "recorder" {
-  source = "../"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-}
-
-module "role" {
-  source = "../"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-  
-}
-
-module "rules" {
-  source = "../"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-  
-}
-
-module "sns" {
-  source = "../"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-  
-}
-
-
 resource "random_string" "random" {
     length  =   8
     special =   false
