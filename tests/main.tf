33bb3bda5b1f23904e4aa9b9aca9cc5b43f34b16
Daniel Navarro <danielnavarro@Daniels-MacBook-Pro.local>
2020-03-06 14:43:36 +1100

    Fix formatting

--- tests/main.tf
+++ tests/main.tf
@@ -1,17 +1,17 @@
 module "aggregator" {
   source = "../"
-  
-  aggregator_account_id     =   "354334841216"
-  aggregator_account_region =   "ap-southeast-2"
-  bucket_name   =   "cmdlab-sandpit2-${random_string.random.result}"
-  is_aggregator = true
+
+  aggregator_account_id     = "354334841216"
+  aggregator_account_region = "ap-southeast-2"
+  bucket_name               = "cmdlab-sandpit2-${random_string.random.result}"
+  is_aggregator             = true
 }
 
 resource "random_string" "random" {
-    length  =   8
-    special =   false
-    upper   =   false
-  
+  length  = 8
+  special = false
+  upper   = false
+
 }
 
 
