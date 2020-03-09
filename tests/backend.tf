e9f3a18adaa88f4d7a227fb6259ab489beff850c
Daniel Navarro <danielnavarro@Daniels-MacBook-Pro.local>
2020-03-09 14:44:46 +1100

    Updated Backend Vars

--- tests/backend.tf
+++ tests/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket                  = "cmdlab-sandpit2-terraform-backend"
-    key                     = "module-cmdlab-sandpit2-tf-aws-config"
+    bucket                  = "cmdlabtf-terraform-backend"
+    key                     = "module-cmd-tf-aws-config"
     region                  = "ap-southeast-2"
-    profile                 = "cmdlab-sandpit2"
-    dynamodb_table          = "cmdlab-sandpit2-terraform-lock"
+    profile                 = "cmdlabtf-tfbackend"
+    dynamodb_table          = "cmdlabtf-terraform-lock"
     skip_metadata_api_check = true
   }
 }
