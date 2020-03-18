fe4d63755bad93d189fbcd285b14a932db6fe279
Daniel Navarro <danielnavarro@Daniels-MacBook-Pro.local>
2020-03-18 14:31:13 +1100

    UPDATE sandpit2 provider

--- tests/provider.tf
+++ tests/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = "ap-southeast-2"
   version                 = "~> 2.0"
-  profile                 = "cmdlab-sandpit"
+  profile                 = "cmdlab-sandpit2"
   skip_metadata_api_check = true
 }
